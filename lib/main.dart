cd4f4b31e4dc3000cb9a9743be04901f6c560ec4
Rashini Hansika <80822074+RashiniHansika@users.noreply.github.com>
2022-11-09 08:26:22 +0530

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,22 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:uee_assignment/screens/home/home.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  runApp( MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
-
-  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+  const MyApp({super.key});
 
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _initialization,
-      builder: (context, snapshot)
-      {
-        if(snapshot.connectionState == ConnectionState.waiting)
-        {
-          return const MaterialApp(
-            home: Scaffold(
-              body: Center(
-                child: Text('career builder app is beign initialized',
-                style: TextStyle(
-                  color: Colors.cyan,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-              ),
-            ),
-          );
-        }
-        else if(snapshot.hasError)
-        {
-          return const MaterialApp(
-            home: Scaffold(
-              body: Center(
-                child: Text('An error has been occurred',
-                style: TextStyle(
-                  color: Colors.cyan,
-                  fontSize: 40,
-                  fontWeight: FontWeight.bold
-                ),
-                ),
-              ),
-            ),
-          );
-        }  
-        return MaterialApp(
-          debugShowCheckedModeBanner: false,
-          title: 'career builder app',
-          theme: ThemeData(
-            scaffoldBackgroundColor: Colors.black,
-            primarySwatch: Colors.blue,
-          ),
-          home: const Scaffold(),
-        );
-      }
+    SystemChrome.setSystemUIOverlayStyle(
+      SystemUiOverlayStyle(statusBarColor: Colors.transparent));
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage()
     );
   }
-  
 }
-
